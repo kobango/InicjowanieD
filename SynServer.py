1bf65658ac7d04c4d374f7a1b3c3f392adf18a42
Ryszard <kamilszkaradnik@gmail.com>
2019-03-15 23:57:08 +0100

    my edit

--- SynServer.py
+++ SynServer.py
@@ -7,13 +7,8 @@ async def hello(websocket, path):
     name = await websocket.recv()
     print(f"< {name}")
 
-    walue = danalise.spliting(danalise.txt)
-    walue2 = danalise.spliting(danalise.txt2)
-    walue3 = danalise.spliting(danalise.txt3)
-    array = danalise.windowmake(walue)
-    array = danalise.windowpeek(array, walue2)
-    array = danalise.windowpeek(array, walue3)
-    array = danalise.windowpeek(array, walue2)
+
+    array = [danalise.txt,danalise.txt2,danalise.txt2,danalise.txt,danalise.txt3]
     while True:
         data = array[random.randrange(4)]
         await websocket.send(data)
