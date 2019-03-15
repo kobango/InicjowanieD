733408a94d2debacadb2237a9cdf3bfca943cebf
grobocop12 <chasnik97876@gmail.com>
2019-03-16 00:05:21 +0100

    odczytywanie dwóch wartości i odczytywanie listy

--- input.py
+++ input.py
@@ -5,14 +5,22 @@ host = 'ws://192.168.102.59:1234/'
 ws = websockets.connect(host)
 
 
+async def __get_input_list_async(host_name, length):
+    async with websockets.connect(host_name) as websocket:
+        values = []
+        for i in range(length):
+            values.append(await websocket.recv())
+        return values
+
 async def __get_input_async(host_name):
     async with websockets.connect(host_name) as websocket:
-        a = await websocket.recv()
-        b = await websocket.recv()
-        return a, b
+        first = await websocket.recv()
+        second = await websocket.recv()
+        return first, second
+        
 
 '''
-a , b = asyncio.get_event_loop().run_until_complete(__get_input_async(host))
+a, b = asyncio.get_event_loop().run_until_complete(__get_input_async(host))
 
 print(a, b)
 '''
