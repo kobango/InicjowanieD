88a250842b821a12b65a476f98cf27cd59d38d5c
grobocop12 <chasnik97876@gmail.com>
2019-03-15 23:43:25 +0100

    asynchroniczne wejście

--- input.py
+++ input.py
@@ -1,13 +1,21 @@
 import asyncio
 import websockets
 
-
 host = 'ws://192.168.102.59:1234/'
-async def hello():
-    async with websockets.connect(host) as ws:
-        while True:
-            data = await ws.recv()
-            print(data)
-            #print(type(data))
+ws = websockets.connect(host)
 
-asyncio.get_event_loop().run_until_complete(hello())
+
+async def __get_input_async(host_name):
+    async with websockets.connect(host_name) as websocket:
+        while True:
+            a = await websocket.recv()
+            print(a)
+        
+
+def get_input(host_name):
+    a = asyncio.get_event_loop().run_until_complete(__get_input_async(host_name))
+    return a
+
+asyncio.get_event_loop().run_until_complete(__get_input_async(host))
+
+
